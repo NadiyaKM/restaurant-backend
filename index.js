b84b8323a4bbbbb050ab8576f197e8c0822bbd01
NadiyaKM <atsska2433@gmail.com>
2023-10-10 20:34:58 +0530

    restaurant-app

---
+++ index.js
@@ -0,0 +1,25 @@
+//1 import json-server
+const jsonserver=require('json-server')  
+
+
+//2 create a server for media app
+const server = jsonserver.create()
+
+
+//3 set up path for db.json file
+const router = jsonserver.router('db.json')
+
+//4 return a middleware used by json-sever
+const middleware = jsonserver.defaults()
+
+//5 setup a port number
+const port = process.env.PORT || 3001
+
+//6 use in server
+server.use(middleware)
+server.use(router)
+
+//7 to run the port
+server.listen(port ,()=>{
+    console.log('listening on port' +port);
+})
